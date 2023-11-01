4f9c7fb834f1e73d594c610bc92e324e1498a59d
linitha123 <linitha384@gmail.com>
2023-11-01 05:53:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let App = () => {
+    let [advise,setAdvise] = useState('')
+
+    let fetchData = async ()=>{
+        let fetchInput =  await fetch('https://api.adviceslip.com/advice')
+        let result = await fetchInput.json()
+     let {advice} =  (result.slip)
+     setAdvise(advice)
+ 
+
+    }
+  
+   
+   useEffect(()=>{
+       fetchData()
+         },[])
+       
+        
+    return(
+<div className='app'>
+       <div className='container'>
+       <h1>{advise}</h1>
+       <button onClick={fetchData}>get</button>
+       </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
